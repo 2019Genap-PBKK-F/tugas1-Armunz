e13dcb3f0e2019e5af62f49c7fda78bad60b2411
Dwi Prasetya Armunanta <armunantadwiprasetya@gmail.com>
2020-02-12 06:09:30 +0700

    kurang koneksi ke database
    kurang koneksi ke database

---
+++ src/components/views/Excel.vue
@@ -0,0 +1,32 @@
+<template>
+    <div><input type="button" value="Add new row" class="btn btn-info" @click="() => spreadsheet.insertRow()" /></div>
+</template>
+
+<script>
+import jexcel from 'jexcel'
+import 'jexcel/dist/jexcel.css'
+var data = [
+  ['Dwi Prasetya Armunanta', '05111740000025', '1998-05-10', '', true],
+  ['Hendra Ramadani', '05111740000055', '1998-29-10', '', true]
+]
+
+var options = {
+  data: data,
+  allowToolbar: true,
+  columns: [
+    { type: 'text', title: 'Nama Mahasiswa', width: '120px' },
+    { type: 'numeric', title: 'NRP', width: '250px' },
+    { type: 'calendar', title: 'Tanggal Lahir', width: '250px' },
+    { type: 'image', title: 'Photo', width: '120px' },
+    { type: 'checkbox', title: 'Status', width: '80px' }
+  ]
+}
+
+export default {
+  name: 'Jexcel',
+  mounted: function () {
+    let spreadsheet = jexcel(this.$el, options)
+    Object.assign(this, { spreadsheet })
+  }
+}
+</script>
